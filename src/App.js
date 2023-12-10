7b1562d57c739f6f3e643376c57f06d529191a8e
netef <38144727+netef@users.noreply.github.com>
2023-12-10 21:29:06 +0200

    added the grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import BooksGrid from "./components/booksGrid/booksGrid";
+import "./page.css";
 function App() {
   return (
-    <div>
+    <div className="page-layout">
       <h1>app</h1>
+      <BooksGrid />
     </div>
   );
 }
---
+++ src/components/booksGrid/booksGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./booksGrid.css";
+export default function BooksGrid() {
+  return (
+    <div className="books-grid">
+      <p>wow</p>
+      <p>wow</p>
+      <p>wow</p>
+      <p>wow</p>
+      <p>wow</p>
+    </div>
+  );
+}
