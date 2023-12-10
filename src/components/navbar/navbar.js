76fe259f64d6f03516d045c3af82925dea51041d
netef <38144727+netef@users.noreply.github.com>
2023-12-10 21:12:12 +0200

    navbar in place

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navbar.css";
+export default function Navbar() {
+  return (
+    <div className="container">
+      <h2>Roy's Library</h2>
+      <h2>O</h2>
+    </div>
+  );
+}
