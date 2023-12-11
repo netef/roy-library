315bd14ffde9dfb4a7aff181bee42059ab3286ce
netef <38144727+netef@users.noreply.github.com>
2023-12-11 21:45:19 +0200

    added modal and seperated the components and props

--- src/components/bookGridItem/bookGridItem.js
+++ src/components/bookGridItem/bookGridItem.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./bookGridItem.css";
-export default function BookGridItem() {
-  return (
-    <div className="book-container">
-      <img
-        className="book-img"
-        src="https://content.wepik.com/statics/90897927/preview-page0.jpg"
-        alt="book cover"
-      />
-      <h3>Spring Book</h3>
-      <p>4 available</p>
-    </div>
-  );
+export default function BookGridItem({ book }) {
+    return (
+        <div className="book-container">
+            <img className="book-img" src={book.img} alt="book cover" />
+            <h2>{book.title}</h2>
+            <h3>{book.description}</h3>
+            <p>Amount available: {book.quantity}</p>
+        </div>
+    );
 }
