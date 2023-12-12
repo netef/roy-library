995ec20331c3a5ba7ad6908d85e031d5a75d757b
netef <38144727+netef@users.noreply.github.com>
2023-12-12 22:31:03 +0200

    added register and login pages

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./login.css";
+import { Link } from "react-router-dom";
+import { login, register } from "../../components/utils/constants";
 
 export default function Login() {
-    return <div>login</div>;
+    const [loading, setLoading] = useState(false);
+    const registerUser = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const data = {
+            email: e.target.email.value,
+            password: e.target.password.value,
+        };
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    };
+    return (
+        <div className="page-layout center">
+            <h1>Please login to use the library.</h1>
+            <form id="form" className="contactForm" onSubmit={registerUser}>
+                <label>Email: </label>
+                <input name="email" type="text" required />
+                <label>Password: </label>
+                <input name="password" type="password" required />
+
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <button type="submit">submit</button>
+                )}
+            </form>
+
+            <p>
+                Not registered?{" "}
+                <Link style={{ color: "blue" }} to={register}>
+                    click to register
+                </Link>
+            </p>
+        </div>
+    );
 }
